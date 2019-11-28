4d1967c478216f0d1f6de550d5bec330f376a668
Lucas Donadi <lucasdonadi@gmail.com>
2019-11-28 17:07:58 -0300

    Add files via upload
    Atualizando funções

--- aps_ed2.c
+++ aps_ed2.c
@@ -2,13 +2,16 @@
 #include <stdio.h>
 #include <string.h>
 #include "aps_ed2.h"
+#include <ctype.h>
 
 int main(int argc, char *argv[ ]){
 
-    BubbleSort_string(argv[1], strlen(argv));
+    printf("\n Palavra ordenada:%s", argv[1]);
+    
+    ordena_palavra(argv[1], strlen(argv[1]));
 
     printf("\n Palavra ordenada:%s", argv[1]);
 
 
-return 0;    
+return 0;
 }
--- aps_ed2.h
+++ aps_ed2.h
@@ -1,17 +1,56 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-void BubbleSort_string(char * word, int n){ 
-    char aux[n]; 
-  
-    for (int j=0; j<n-1; j++) { 
-        for (int i=j+1; i<n; i++){ 
-            if (strcmp(word[j], word[i]) > 0){ 
-                strcpy(aux, word[j]); 
-                strcpy(word[j], word[i]); 
-                strcpy(word[i], aux); 
-            } 
-        } 
-    } 
-} 
+/*void BubbleSort_string(char * word, int n){
+    char aux;
+    int i, j;
+
+    printf("\n palavra inicial: %s", word);
+
+    for (j=0; j<n-1; j++) {
+        for (i=j+1; i<n-1; i++){
+            if (strcmp(word[j], word[i]) > 0){
+                strcpy(aux, word[j]);
+                strcpy(word[j], word[i]);
+                strcpy(word[i], aux);
+            }
+        }
+    }
+}*/
+
+void ordena_palavra(char * word, int n){
+    char aux[n];
+    int i, j, cont=0;
+
+    for(i=0; i<n-1;){
+        for(j=i+1; j<n-1;){
+            if(word[i]>=word[j] || i>n){
+                aux[cont]=word[j];
+                j++;
+            }
+            else if(word[j]>word[i] || j>n){
+                aux[cont]=word[i];
+                i++;
+            }
+            cont++;
+        }
+    }
+
+    strcpy(aux, word);
+}
+
+/*void BubbleSort_2(char * word, int n){
+char aux[n];
+int i, j;
+
+for(i=1;i<5;++i){
+    for(j=4;j>=i;--j){
+        if (strcmp(word[j-1],word[j])>0){
+            aux=word[j-1];
+            word[j-1]=word[j];
+            word[j]=aux;
+        }
+    }
+}*/
