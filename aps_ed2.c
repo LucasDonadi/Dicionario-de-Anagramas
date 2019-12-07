3b19dc25a2fa23e9400303da7901f0503810f665
Lucas Donadi <lucasdonadi@gmail.com>
2019-12-07 17:10:48 -0300

    implementei a impressão e as listas de palavras
    implementei a impressão e as listas de palavras para cada posicao do vetor do dicionario

--- aps_ed2.c
+++ aps_ed2.c
@@ -7,9 +7,8 @@
 int main(int argc, char *argv[ ]){
     int i;
 
-    //quant de palavras = 261800
-    ordena_palavra(argv[1], strlen(argv[1]));
-    printf("\n Palavra ordenada:%s", argv[1]);
+    //ordena_palavra(argv[1], strlen(argv[1]));
+    //printf("\n Palavra ordenada:%s", argv[1]);
 
     FILE *fp = fopen ("br.txt", "r");
     if (fp == NULL) {
@@ -29,20 +28,26 @@ int main(int argc, char *argv[ ]){
         ordena_palavra(aux->palavra_ordenada, strlen(aux->palavra_ordenada));
         aux->chave=func_hash(aux->palavra_ordenada, strlen(aux->palavra_ordenada));
 
-        if(dicionario->cont==0){
+        if(dicionario[aux->chave].cont<100){
             dicionario[aux->chave].chave=aux->chave;
-            strcpy(dicionario[aux->chave].palavra,aux->palavra);
-            strcpy(dicionario[aux->chave].palavra_ordenada,aux->palavra_ordenada);
+            dicionario[aux->chave].vetor_palavra[cont].chave=chave=aux->chave;
+            strcpy(dicionario[aux->chave].vetor_palavra[cont].palavra,aux->palavra);
+            strcpy(dicionario[aux->chave].vetor_palavra[cont].palavra_ordenada,aux->palavra_ordenada);
+            dicionario[aux->chave].cont++;
         }
-        else if(dicionario->cont<100){
-            dicionario[aux->chave].chave=aux->chave;
-            strcpy(dicionario[aux->chave].palavra,aux->palavra);
-            strcpy(dicionario[aux->chave].palavra_ordenada,aux->palavra_ordenada);
-        }
+
         printf("\nPalavra '%s' adicionada ao dicionario!", dicionario[aux->chave].palavra);
         printf("\nPalavra do arquivo ordenada:%s", dicionario[aux->chave].palavra_ordenada);
         printf("\nCodigo Hash da palavra %s: %d\n", dicionario[aux->chave].palavra, dicionario[aux->chave].chave);
     }
+
+    printf("\n\n Anagramas a partir da palavra: %s", argv[1]);
+    aux->chave=func_hash(argv[1], strlen(argv[1]));
+
+    for(i=0; i<dicionario[aux->chave].cont; i++){
+        printf("\n%s", dicionario[aux->chave].vetor_palavra[i].palavra);
+    }
+
 
 
     fclose(fp);
